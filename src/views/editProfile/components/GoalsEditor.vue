b29f6b90ccc1b78b0b373bb4ba06a560a7ab18e3
Baxrom Avliyoqulov <avliyoqulovbaxrom99@gmail.com>
2026-07-15 23:51:47 +0500

    fix: add box-sizing border-box to prevent inputs from overflowing container

--- src/views/editProfile/components/GoalsEditor.vue
+++ src/views/editProfile/components/GoalsEditor.vue
@@ -181,6 +181,10 @@ export default {
 </script>
 
 <style scoped>
+.goals-editor-wrap, .goals-editor-wrap * {
+  box-sizing: border-box;
+}
+
 .goals-editor-wrap {
   display: flex;
   flex-direction: column;
@@ -413,6 +417,7 @@ export default {
 .group-label i { color: #94a3b8; }
 
 .goal-input {
+  box-sizing: border-box;
   width: 100%;
   padding: 0.85rem 1rem;
   font-size: 0.95rem;
